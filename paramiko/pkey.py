ploxiln/paramiko-ng

commit 40daf204b339b8f73e7389a20e9b7915d671f58b

pkey: __eq__() should not use hash()
... because `hash()` is too weak and not intended for this.
Also simplify `__cmp__()` (which did work fine).

This fixes a security flaw. If you are using Paramiko with Python 2,
or a Python 3 which is running with PYTHONHASHSEED=0, it is possible
for an attacker to craft a new keypair from an exfiltrated public key,
which Paramiko would consider equal to the original key.

This could enable attacks such as:
 * Paramiko server processes would incorrectly authenticate the attacker
   (using their generated private key) as if they were the victim.
 * Paramiko client processes would incorrectly validate a connected
   server (when host key verification is enabled) while subjected to a
   man-in-the-middle attack. This impacts more users than the server-side
   version, but also carries higher requirements for the attacker, namely
   successful DNS poisoning or other MITM techniques.

Reported by @jun66j5 in https://github.com/paramiko/paramiko/issues/908
Vulnerability description by Jeff Forcier <jeff@bitprophet.org>

paramiko/pkey.py
@@ -209,26 +209,20 @@ class PKey(object):
     def __str__(self):
         return self.asbytes()
 
-    # noinspection PyUnresolvedReferences
-    # TODO: The comparison functions should be removed as per:
-    # https://docs.python.org/3.0/whatsnew/3.0.html#ordering-comparisons
     def __cmp__(self, other):
+        # python-2 only, same purpose as __eq__()
+        return cmp(self.asbytes(), other.asbytes())  # noqa
+
+    def __eq__(self, other):
         """
-        Compare this key to another.  Returns 0 if this key is equivalent to
-        the given key, or non-0 if they are different.  Only the public parts
+        Compare this key to another.  Returns True if this key is equivalent to
+        the given key, or False if they are different.  Only the public parts
         of the key are compared, so a public key will compare equal to its
         corresponding private key.
 
         :param .PKey other: key to compare to.
         """
-        hs = hash(self)
-        ho = hash(other)
-        if hs != ho:
-            return cmp(hs, ho)   # noqa
-        return cmp(self.asbytes(), other.asbytes())  # noqa
-
-    def __eq__(self, other):
-        return hash(self) == hash(other)
+        return self.asbytes() == other.asbytes()
 
     def get_name(self):
         """

commit 5d3aadcd67720a193c34eea87739d5dcd60e3905

pkey: fix PKey.__cmp__() against None for python2
A bug introduced by 40daf20 (added in v2.8.8)
and fixed for python3 in ca3839e

paramiko/pkey.py
@@ -217,7 +217,8 @@ class PKey(object):
 
     def __cmp__(self, other):
         # python-2 only, same purpose as __eq__()
-        return cmp(self.asbytes(), other.asbytes())  # noqa
+        other_asbytes = other.asbytes() if isinstance(other, PKey) else b""
+        return cmp(self.asbytes(), other_asbytes)  # noqa
 
     def __eq__(self, other):
         """

tests/test_pkey.py
@@ -183,6 +183,9 @@ class KeyTest(unittest.TestCase):
         key2 = RSAKey.from_private_key(s)
         self.assertEqual(key, key2)
 
+        # ensure comparison does not raise exception
+        assert key != None  # noqa: E711
+
     def test_load_rsa_password(self):
         key = RSAKey.from_private_key_file(_support('test_rsa_password.key'), 'television')
         self.assertEqual('ssh-rsa', key.get_name())
@@ -207,6 +210,9 @@ class KeyTest(unittest.TestCase):
         s.seek(0)
         key2 = DSSKey.from_private_key(s)
         self.assertEqual(key, key2)
+
+        # ensure comparison does not raise exception
+        assert key != None  # noqa: E711
 
     def test_load_dss_password(self):
         key = DSSKey.from_private_key_file(_support('test_dss_password.key'), 'television')
@@ -319,6 +325,9 @@ class KeyTest(unittest.TestCase):
         s.seek(0)
         key2 = ECDSAKey.from_private_key(s)
         self.assertEqual(key, key2)
+
+        # ensure comparison does not raise exception
+        assert key != None  # noqa: E711
 
     def test_load_ecdsa_password_256(self):
         key = ECDSAKey.from_private_key_file(_support('test_ecdsa_password_256.key'),
@@ -521,6 +530,9 @@ class KeyTest(unittest.TestCase):
             _support('test_ed25519_password.key'), b'abc123'
         )
         self.assertNotEqual(key1.asbytes(), key2.asbytes())
+
+        # ensure comparison does not raise exception
+        assert key1 != None  # noqa: E711
 
     @pytest.mark.skipif("not Ed25519Key.is_supported()")
     def test_ed25519_nopad(self):